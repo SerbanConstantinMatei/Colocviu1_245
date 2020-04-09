39253d9ac6aea76903468522b9b6370d4841ecd2
root <root@DESKTOP-KT6PDSJ.localdomain>
2020-04-09 14:00:30 +0300

    B1

--- app/src/main/java/ro/pub/cs/systems/eim/colocviu1_245/MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/colocviu1_245/MainActivity.java
@@ -3,6 +3,7 @@ package ro.pub.cs.systems.eim.colocviu1_245;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -21,6 +22,36 @@ public class MainActivity extends AppCompatActivity {
         add = (Button)findViewById(R.id.button);
         compute = (Button)findViewById(R.id.button2);
 
-        text.setText(String.valueOf(0));
+        text.setText("");
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case (R.id.button):
+                        String val = text.getText().toString();
+                        try {
+                            int term = Integer.parseInt(val);
+                            String seq = result.getText().toString();
+
+                            if (seq.equals("")) {
+                                seq = val;
+                            } else {
+                                seq = seq.concat(" + ");
+                                seq = seq.concat(val);
+                            }
+
+                            result.setText(seq);
+                        } catch (NumberFormatException e) {
+                            break;
+                        }
+
+                        break;
+                }
+            }
+        };
+
+        add.setOnClickListener(listener);
+        compute.setOnClickListener(listener);
     }
 }
